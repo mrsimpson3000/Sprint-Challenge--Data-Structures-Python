2eba75bdbf1b124c8b2a5bcff83ced94bf9b574a
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-07-31 23:28:41 -0400

    Finished ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.content = []
+        self.replace = -1
 
     def append(self, item):
-        pass
+        if len(self.content) < self.capacity:
+            self.content.append(item)
+        else:
+            if self.replace >= self.capacity - 1:
+                self.replace = 0
+            else:
+                self.replace += 1
+            self.content[self.replace] = item
 
     def get(self):
-        pass
+        return self.content
